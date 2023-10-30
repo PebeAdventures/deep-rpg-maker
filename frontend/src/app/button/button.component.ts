a4c8747f80dad23ea2c0c9d9df002f3c9f7b9a3b
Diobel <4510471diobel@gmail.com>
2023-10-30 15:17:20 +0100

    for review

--- frontend/src/app/button/button.component.ts
+++ frontend/src/app/button/button.component.ts
@@ -6,8 +6,8 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./button.component.css']
 })
 export class ButtonComponent {
-  //@Input() name?: string;
-  name: string = 'button testowy';
+  @Input() name?: string;
+  //name: string = 'button testowy';
   button1Clicked() {
     // Handle button 1 click event
     // You can add your logic here
